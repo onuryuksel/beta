f807453bbcde8b8280fdadffb177d7578ff02a5e
onuryuksel <onuryuksel@me.com>
2025-05-05 18:46:18 +0400

    Update combined_extractor_app.py

--- combined_extractor_app.py
+++ combined_extractor_app.py
@@ -1560,6 +1560,10 @@ else:
         else: st.session_state.df_comparison_sorted = pd.DataFrame(); print("Comparison skipped.")
         st.rerun()
         df_ounass_live = st.session_state.get('df_ounass'); df_competitor_live = st.session_state.get('df_competitor'); df_comparison_sorted_live = st.session_state.get('df_comparison_sorted'); live_competitor_name = st.session_state.competitor_selection
+        df_ounass_live            = st.session_state.get("df_ounass")
+        df_competitor_live        = st.session_state.get("df_competitor")
+        df_comparison_sorted_live = st.session_state.get("df_comparison_sorted")
+        live_competitor_name      = st.session_state.competitor_selection
         display_all_results(
             df_ounass_live,
             df_competitor_live,
